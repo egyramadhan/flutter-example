f7fa8b229b2da966ea49371b3e9ba9c43b1d5506
egyramadhan <36892796+egyramadhan@users.noreply.github.com>
2020-08-04 20:17:46 +0700

    Update main.dart

--- flutter_example/lib/main.dart
+++ flutter_example/lib/main.dart
@@ -2,50 +2,128 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int number = 0;
-  void tekanTombol() {
-    setState(() {
-      number = number + 1;
-    });
-  }
-
-  void tekanTombolKurang() {
-    setState(() {
-      number = number - 1;
-    });
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("App Demo Testing Belajar "),
+          title: Text("Stack"),
         ),
-        body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        body: Stack(
           children: <Widget>[
-            Text(
-              number.toString(),
-              style: TextStyle(fontSize: 10 + number.toDouble()),
+            //background
+            Column(
+              children: <Widget>[
+                Flexible(
+                  flex: 1,
+                  child: Row(
+                    children: <Widget>[
+                      Flexible(
+                        flex: 1,
+                        child: Container(
+                          color: Colors.white,
+                        ),
+                      ),
+                      Flexible(
+                        flex: 1,
+                        child: Container(
+                          color: Colors.black12,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Flexible(
+                  flex: 1,
+                  child: Row(
+                    children: <Widget>[
+                      Flexible(
+                        flex: 1,
+                        child: Container(
+                          color: Colors.black12,
+                        ),
+                      ),
+                      Flexible(
+                        flex: 1,
+                        child: Container(
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
             ),
-            RaisedButton(
-              child: Text("Tambah"),
-              onPressed: tekanTombol,
+            ListView(
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.all(10.0),
+                      child: Text(
+                          "lowdrakjrjwa walkjwalkjwa wdlkwahkdhkwajhdkjwahdkwajdkjwa",
+                          style: TextStyle(fontSize: 30)),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(10.0),
+                      child: Text(
+                          "lowdrakjrjwa walkjwalkjwa wdlkwahkdhkwajhdkjwahdkwajdkjwa",
+                          style: TextStyle(fontSize: 30)),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(10.0),
+                      child: Text(
+                          "lowdrakjrjwa walkjwalkjwa wdlkwahkdhkwajhdkjwahdkwajdkjwa",
+                          style: TextStyle(fontSize: 30)),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(10.0),
+                      child: Text(
+                          "lowdrakjrjwa walkjwalkjwa wdlkwahkdhkwajhdkjwahdkwajdkjwa",
+                          style: TextStyle(fontSize: 30)),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(10.0),
+                      child: Text(
+                          "lowdrakjrjwa walkjwalkjwa wdlkwahkdhkwajhdkjwahdkwajdkjwa",
+                          style: TextStyle(fontSize: 30)),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(10.0),
+                      child: Text(
+                          "lowdrakjrjwa walkjwalkjwa wdlkwahkdhkwajhdkjwahdkwajdkjwa",
+                          style: TextStyle(fontSize: 30)),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(10.0),
+                      child: Text(
+                          "lowdrakjrjwa walkjwalkjwa wdlkwahkdhkwajhdkjwahdkwajdkjwa",
+                          style: TextStyle(fontSize: 30)),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(10.0),
+                      child: Text(
+                          "lowdrakjrjwa walkjwalkjwa wdlkwahkdhkwajhdkjwahdkwajdkjwa",
+                          style: TextStyle(fontSize: 30)),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(10.0),
+                      child: Text(
+                          "lowdrakjrjwa walkjwalkjwa wdlkwahkdhkwajhdkjwahdkwajdkjwa",
+                          style: TextStyle(fontSize: 30)),
+                    ),
+                  ],
+                )
+              ],
             ),
-            RaisedButton(
-              child: Text("Kurang"),
-              onPressed: tekanTombolKurang,
-            ),
+            Align(
+              alignment: Alignment(0.8, 0.8),
+              child: RaisedButton(
+                  child: Text("Button"), color: Colors.amber, onPressed: () {}),
+            )
           ],
-        )),
+        ),
       ),
     );
   }
